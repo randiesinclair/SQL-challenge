7ff3cf423a20872eede8a7ef77da5ed7d96d7a82
randiesinclair <109693942+randiesinclair@users.noreply.github.com>
2022-11-30 22:47:14 -0600

    update ERD/finalize schema and query

--- EmployeeSQL/emp_schema.sql
+++ EmployeeSQL/emp_schema.sql
@@ -8,16 +8,16 @@ drop table titles;
 --Create departments table
 
 CREATE TABLE departments (
-dept_no varchar PRIMARY KEY,
-dept_name varchar
+dept_no varchar(4) PRIMARY KEY,
+dept_name varchar(20) NOT NULL
 );
 	
 select * from departments;
 
 -- Create dept_emp table
 CREATE TABLE dept_emp (
-emp_no varchar,
-dept_no varchar,
+emp_no varchar(6) NOT NULL,
+dept_no varchar(4) NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
@@ -26,8 +26,8 @@ select * from dept_emp;
 
 -- Create dept_manager table
 CREATE TABLE dept_manager (
-dept_no varchar,
-emp_no varchar PRIMARY KEY,
+dept_no varchar(4) NOT NULL,
+emp_no varchar(6) NOT NULL PRIMARY KEY,
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
@@ -36,13 +36,13 @@ select * from dept_manager;
 
 -- Create employees table
 CREATE TABLE employees (
-emp_no varchar,
-emp_title varchar,
-birth_date date,
-first_name varchar,
-last_name varchar,
-sex varchar,
-hire_date date,
+emp_no varchar(6) NOT NULL,
+emp_title varchar(5) NOT NULL,
+birth_date date NOT NULL,
+first_name varchar(20) NOT NULL,
+last_name varchar(20) NOT NULL,
+sex varchar(1) NOT NULL,
+hire_date date NOT NULL,
 PRIMARY KEY (emp_no),
 FOREIGN KEY (emp_title) REFERENCES titles (title_id)
 );
@@ -51,8 +51,8 @@ select * from employees;
 
 -- Create salaries table
 CREATE TABLE salaries (
-emp_no varchar PRIMARY KEY,
-salary int,
+emp_no varchar(6) NOT NULL PRIMARY KEY,
+salary int NOT NULL,
 FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
@@ -60,8 +60,8 @@ select * from salaries;
 
 -- Create titles table
 CREATE TABLE titles (
-title_id varchar PRIMARY KEY,
-title varchar
+title_id varchar(5) NOT NULL PRIMARY KEY,
+title varchar(20) NOT NULL
 );
 
 select * from titles;
